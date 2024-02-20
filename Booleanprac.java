45957a5f252c71008e38e296f03b478217dd0aee
Ansh Singh <156088039+shadowhawk33@users.noreply.github.com>
2024-02-20 15:10:18 +0530

    Add files via upload

---
+++ Booleanprac.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Booleanprac {
+    public static void main(String[] args) {
+    boolean isJavaFun = true;
+    boolean isFishTasty = false;    
+    System.out.println(isJavaFun);
+    System.out.println(isFishTasty);
+    }
+}
+    
